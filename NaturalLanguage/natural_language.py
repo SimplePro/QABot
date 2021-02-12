5a2c0b3e92e0f663374c76107bfa8ad792f7c04a
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-02-12 20:10:28 +0900

    added natural_langauge module file

--- NaturalLanguage/natural_language.py
+++ NaturalLanguage/natural_language.py
@@ -1,5 +1,6 @@
 from gensim.summarization import keywords
 from nltk.tokenize import sent_tokenize
+import nltk
 import numpy as np
 from numpy.linalg import norm
 import wikipedia
@@ -21,6 +22,7 @@ class NaturalLanguageProcessing:
         self.sentences = []  # 본문 속 문장
         self.sentences_vec = []  # 본문 속 문장별 벡터라이저
         self.emotion = 0.5  # 감정
+        self.pos = []
 
     def reset(self, s):
         self.s = s
@@ -67,6 +69,13 @@ class NaturalLanguageProcessing:
         self.keywords_list = list(keywords(self.s, lemmatize=False).split("\n"))
         return self.keywords_list
 
+    # 명사 추출
+    def get_pos(self):
+        words = nltk.word_tokenize(self.s)
+        pos = nltk.pos_tag(words)
+        self.pos = [p[0] for p in pos if p[1] in ["NN", "NNP"]]
+        return self.pos
+
 
 if __name__ == '__main__':
     title = input("search: ")
@@ -75,6 +84,7 @@ if __name__ == '__main__':
         print(i)
     index = int(input("What do you want? (Enter the desired order): ")) - 1
     text = wikipedia.summary(search[index])
-    natural_language_summary = NaturalLanguageProcessing(s=text)
-    print(natural_language_summary.get_summary())
-    print(natural_language_summary.get_keywords())
+    natural_language_processing = NaturalLanguageProcessing(s=text)
+    print(natural_language_processing.get_summary())
+    print(natural_language_processing.get_keywords())
+    print(natural_language_processing.get_pos())
